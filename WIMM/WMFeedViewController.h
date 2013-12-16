58f8df887f3ce1bc20927166c84a1649c06ef25c
Andrey Pachay <apachay@octoberry.ru>
2013-12-16 18:59:04 +0400

    UI beta

---
+++ WIMM/WMFeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  WMFeedViewController.h
+//  WIMM
+//
+//  Created by Andrey Pachay on 12/11/13.
+//  Copyright (c) 2013 Andrey Pachay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMFeedViewController : UITableViewController
+
+@end
